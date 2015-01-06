ed34961c1c0de510586110a357aac8389e7e2ba9
hpages@fhcrc.org <hpages@fhcrc.org@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-01-06 07:48:41 +0000

    update error message issued by now defunct makeSeqnameIds()
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_0/madman/Rpacks/GenomicRanges@98053 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/makeSeqnameIds.R
+++ R/makeSeqnameIds.R
@@ -1,6 +1,6 @@
 makeSeqnameIds <- function(seqnames, X.is.sexchrom=NA)
 {
-     txt <- "'makeSeqnameIds' is deprecated.
+     txt <- "'makeSeqnameIds' is defunct.
            Use 'rankSeqlevels()' in 'GenomeInfoDb' instead." 
     .Defunct("rankSeqlevels", msg=paste(strwrap(txt), collapse="\n"))
     rankSeqlevels(seqnames, X.is.sexchrom=NA)
